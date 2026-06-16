6911e0616484d56db88776afbded5f533e518cd6
alainlemus <alainttlm@gmail.com>
2026-06-15 18:19:00 -0600

    feat: version 1.4.2

--- app.config.ts
+++ app.config.ts
@@ -72,7 +72,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name:    'Consultoría Inmobiliaria',
     slug:    'app-consultoriainmobiliaria',
-    version: '1.1.2',
+    version: '1.4.2',
     orientation: 'portrait',
     icon:    './assets/icon.png',
     userInterfaceStyle: 'light',
